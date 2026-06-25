163783da1b98c3104d301815c5e0ebab9d6358e2
694259017-sys <694259017@webmail.npru.ac.th>
2026-06-25 16:17:09 +0700

    pencil

---
+++ Pencil.java
@@ -0,0 +1,10 @@
+
+
+      void main() {
+        IO.print("Number of Box");
+        int box = Integer.parseInt(IO.readln());
+        IO.print("Number of pencil");
+        int pencil = Integer.parseInt(IO.readln());
+        int total = box * pencil;
+        IO.println("You have" + total + "pencil");
+    }
