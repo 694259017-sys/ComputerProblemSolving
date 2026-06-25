fea511f72339e30bdea8c64712494c49167bceaf
694259017-sys <694259017@webmail.npru.ac.th>
2026-06-25 16:08:58 +0700

    NameAges

---
+++ NameAges.java
@@ -0,0 +1,10 @@
+
+      
+void main() {
+        IO.print("Enter Your name");
+        String name = IO.readln();
+        IO.print("Enter Your ages");
+        int ages = Integer.parseInt(IO.readln());
+        IO.println("Hello" + name + "." + "You are" + ages + "year olds" + ".");
+
+    }
